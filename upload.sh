7fa221248185d0be69d50c568c822319b60dec90
idejie <i@idejie.com>
2017-05-26 23:15:25 +0800

    add a post

--- upload.sh
+++ upload.sh
@@ -5,9 +5,9 @@ hexo d
 cd ./themes/fexo
 git add .
 git commit -m"modify theme"
-git push https://github.com/YangDejie/yangdejie.github.io.git feature/theme
+git push https://github.com/YangDejie/yangdejie.github.io.git feature/theme -f
 cd ../../
 git add .
 git commit -m"add a post $1"
 echo $1
-git push https://github.com/YangDejie/yangdejie.github.io.git develop
+git push https://github.com/YangDejie/yangdejie.github.io.git develop -f
